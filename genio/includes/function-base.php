a83b679736e9824d406ffd9885e3f8824ab6d460
master <rudolifrudolif@gmail.com>
2021-10-12 19:50:12 +0300

    edits

---
+++ genio/includes/function-base.php
@@ -0,0 +1,27 @@
+<?php
+
+//------------------чистка від лишнього ----------------------
+	remove_action('wp_head','feed_links_extra', 3); // убирает ссылки на rss категорий
+	remove_action('wp_head','feed_links', 2); // минус ссылки на основной rss и комментарии
+	remove_action('wp_head','rsd_link');  // сервис Really Simple Discovery
+	remove_action('wp_head','wlwmanifest_link'); // Windows Live Writer
+	// function theme_name_scripts() {
+	// 		wp_enqueue_script( 'jquery' );
+	// }
+	// add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
+	
+
+function remove_menus(){
+  // remove_menu_page( 'index.php' );                  //Консоль
+  remove_menu_page( 'edit.php' );                   //Записи
+  // remove_menu_page( 'upload.php' );                 //Медиафайлы
+  // remove_menu_page( 'edit.php?post_type=page' );    //Страницы
+  remove_menu_page( 'edit-comments.php' );          //Комментарии
+  // remove_menu_page( 'themes.php' );                 //Внешний вид
+  // remove_menu_page( 'plugins.php' );                //Плагины
+  // remove_menu_page( 'users.php' );                  //Пользователи
+  // remove_menu_page( 'tools.php' );                  //Инструменты
+  // remove_menu_page( 'options-general.php' );        //Настройки
+}
+add_action( 'admin_menu', 'remove_menus' );
+
--- genio/includes/script-style.php
+++ genio/includes/script-style.php
@@ -12,15 +12,6 @@
   }
   add_action( 'wp_enqueue_scripts', 'ewa_scripts' );
 
-//------------------подключение шрифтов------------------
-  //  function wph_add_google_fonts() {
-  //      if ( !is_admin() ) {
-  //          wp_register_style('Philosopher', 'https://fonts.googleapis.com/css?family=Neucha|Philosopher:400,400i,700,700i&display=swap&subset=cyrillic&display=swap', array(), null, 'all');
-  //          wp_enqueue_style('Philosopher');
-  //      }
-  //  }
-  //  add_action('wp_enqueue_scripts', 'wph_add_google_fonts');
-
 
 //------------------підключення додаткових функцій для постов ----------------------
   function myfirsttheme_setup() {
