2cbef7ba8b946c8d0938697dab4db82faa69f27b
master <rudolifrudolif@gmail.com>
2021-10-17 20:23:36 +0300

    Аксессуары

--- genio/parts/page-title.php
+++ genio/parts/page-title.php
@@ -1,7 +1,13 @@
 
 <div class="title container">
 	<h1 class="h1"><?php the_title(); ?></h1>
-	<a href="contacts.html">Остались вопросы?</a>
+	<?php 
+		wp_nav_menu( array(
+		'menu'=>'quesction',
+		'menu_class'=>'list',
+		'theme_location'=>'menu',
+		) );
+	?>
 </div>
 
 <div class="border"></div>
